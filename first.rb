449d3a78350a55f8335750a15657ee1e05fa99a4
guptarahul34 <guptarahul34886@gmail.com>
2022-01-08 01:10:22 +0530

    initial coomit

---
+++ first.rb
@@ -0,0 +1,22 @@
+# puts "Hello"
+# 5.times {puts "Rahul Gupta"}
+# str= "myname is rahul"
+# puts str.reverse
+
+student = ["rahul",1,2,3,4,5,6,7]
+len = student.length
+for i in 0..len
+    puts student[i]
+end
+# student.each_with_index {|values,ind| puts "#{values} #{ind}"}
+
+# str = "reel123abc345"
+# puts str.gsub!(/[a-z]/,"")
+
+# student = {
+#     "name"=>"rahul gupta",
+#     "age"=>22,
+#     "address"=>"patel nagar raisen road bhopal"
+# }
+
+# student.each {|key,vaue| puts "#{key} - #{vaue}"}
